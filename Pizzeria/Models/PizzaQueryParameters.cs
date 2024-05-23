d4382360c685b786c413618fa40a933ee1681683
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-23 16:03:43 +0200

    ADD Class PizzaQueryParameters

---
+++ Pizzeria/Models/PizzaQueryParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace Pizzeria.Models
+{
+    public class PizzaQueryParameters
+    {
+        public int? CategoryId { get; set; }
+        public string? Name { get; set; }
+        public string? IngredientName { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+    }
+}
