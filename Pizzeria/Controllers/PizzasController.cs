c97dc233beace5d2a8f13fa74c3665a0b0994b56
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-24 12:12:46 +0200

    REFACTOR action Index of PizzasController

--- Pizzeria/Controllers/PizzasController.cs
+++ Pizzeria/Controllers/PizzasController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Pizzeria.Data;
 using Pizzeria.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Pizzeria.Controllers
 {
@@ -13,34 +14,65 @@ namespace Pizzeria.Controllers
         [HttpGet]
         public IActionResult Index([FromQuery] PizzaQueryParameters queryParameters)
         {
-            List<Pizza> listaPizzas = PizzaManager.GetAllPizzas();
+            using PizzeriaDatabaseContext db = new PizzeriaDatabaseContext();
+
+            var query = db.Pizzas.AsQueryable();
 
             if (queryParameters.CategoryId.HasValue)
             {
-                listaPizzas = listaPizzas.Where(p => p.CategoryId == queryParameters.CategoryId.Value).ToList();
+                query = query.Where(p => p.CategoryId == queryParameters.CategoryId.Value);
             }
 
             if (!string.IsNullOrEmpty(queryParameters.Name))
             {
-                listaPizzas = listaPizzas.Where(p => p.Name.Contains(queryParameters.Name, StringComparison.OrdinalIgnoreCase)).ToList();
+                query = query.Where(p => p.Name.Contains(queryParameters.Name, StringComparison.OrdinalIgnoreCase));
             }
 
             if (!string.IsNullOrEmpty(queryParameters.IngredientName))
             {
-                listaPizzas = listaPizzas.Where(p => p.Ingredients.Any(i => i.Name.Contains(queryParameters.IngredientName, StringComparison.OrdinalIgnoreCase))).ToList();
+                query = query.Where(p => p.Ingredients.Any(i => i.Name.Contains(queryParameters.IngredientName, StringComparison.OrdinalIgnoreCase)));
             }
 
             if (queryParameters.MinPrice.HasValue)
             {
-                listaPizzas = listaPizzas.Where(p => p.Price >= queryParameters.MinPrice.Value).ToList();
+                query = query.Where(p => p.Price >= queryParameters.MinPrice.Value);
             }
 
             if (queryParameters.MaxPrice.HasValue)
             {
-                listaPizzas = listaPizzas.Where(p => p.Price <= queryParameters.MaxPrice.Value).ToList();
+                query = query.Where(p => p.Price <= queryParameters.MaxPrice.Value);
             }
 
+            var listaPizzas = query.ToList();
             return Ok(listaPizzas);
+            //List<Pizza> listaPizzas = PizzaManager.GetAllPizzas();
+
+            //if (queryParameters.CategoryId.HasValue)
+            //{
+            //    listaPizzas = listaPizzas.Where(p => p.CategoryId == queryParameters.CategoryId.Value).ToList();
+            //}
+
+            //if (!string.IsNullOrEmpty(queryParameters.Name))
+            //{
+            //    listaPizzas = listaPizzas.Where(p => p.Name.Contains(queryParameters.Name, StringComparison.OrdinalIgnoreCase)).ToList();
+            //}
+
+            //if (!string.IsNullOrEmpty(queryParameters.IngredientName))
+            //{
+            //    listaPizzas = listaPizzas.Where(p => p.Ingredients.Any(i => i.Name.Contains(queryParameters.IngredientName, StringComparison.OrdinalIgnoreCase))).ToList();
+            //}
+
+            //if (queryParameters.MinPrice.HasValue)
+            //{
+            //    listaPizzas = listaPizzas.Where(p => p.Price >= queryParameters.MinPrice.Value).ToList();
+            //}
+
+            //if (queryParameters.MaxPrice.HasValue)
+            //{
+            //    listaPizzas = listaPizzas.Where(p => p.Price <= queryParameters.MaxPrice.Value).ToList();
+            //}
+
+            //return Ok(listaPizzas);
         }
 
         [HttpGet("{id}")]
